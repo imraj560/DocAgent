d65a0e6301a365df937b66de8b252b60609a89fe
imraj560 <raju560.webdev@gmail.com>
2026-08-14 11:13:03 -0400

    cors changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://docagent-production.up.railway.app";
 
 function App() {
   const [files, setFiles] = useState([]);
